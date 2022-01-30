42987517a61cdfeea2001c092c31f9fa9863be9e
bercivarga <65171545+bercivarga@users.noreply.github.com>
2022-01-30 21:41:15 +0100

    disallow player to jump mid-air

--- main.lua
+++ main.lua
@@ -6,6 +6,8 @@ function love.load()
 	world:addCollisionClass('Platform')
 	world:addCollisionClass('Danger')
 
+	world:setQueryDebugDrawing(true)
+
 	player = world:newRectangleCollider(360, 100, 80, 80, {collision_class = "Player"})
 	player.speed = 240
 	player:setFixedRotation(true)
@@ -21,7 +23,7 @@ function love.update(dt)
 	world:update(dt)
 
 	if player.body then
-		local px, py = player:getPosition()
+		local px = player:getPosition()
 
 		if love.keyboard.isDown('d') then
 			player:setX(px + player.speed * dt)
@@ -41,8 +43,22 @@ function love.draw()
 	world:draw()
 end
 
-function love.keypressed(key, scancode, isrepeat)
+function love.keypressed(key)
 	if key == 'space' or key == 'w' then
-		player:applyLinearImpulse(0, -5000)
+		local colliders = world:queryRectangleArea(player:getX() - 40, player:getY() + 40, 80, 2, {'Platform'})
+
+		if #colliders > 0 then
+			player:applyLinearImpulse(0, -5000)
+		end
 	end
 end
+
+function love.mousepressed(x, y, button)
+	if button == 1 then
+		local colliders = world:queryCircleArea(x, y, 200, {'Platform', 'Danger'})
+		for i,v in ipairs(colliders) do
+			v:destroy()
+		end
+	end
+end
+
